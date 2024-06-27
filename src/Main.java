151fc44fb15642dd8b8e2f6f805877c1138367a8
KhalidDev <148249284+devAlq@users.noreply.github.com>
2024-06-27 20:39:35 +0300

    taskDone

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main{
    public static void main(String[] args) {
-        /*add your code here*/
+        DoublyLinkedList obj = new DoublyLinkedList();
+        for (int x = 1; x<=10; x++){
+           obj.insert(x);
    }
+   System.out.println(obj.binarySearch(8).data);
+
+
+}
 }
